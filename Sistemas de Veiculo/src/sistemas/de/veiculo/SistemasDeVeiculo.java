0df6145d1a60c1f6f34172e178cf074814ae5be8
Camille <ferreiracamille819@gmail.com>
2025-03-10 17:37:49 -0300

    Add files via upload

---
+++ Sistemas de Veiculo/src/sistemas/de/veiculo/SistemasDeVeiculo.java
@@ -0,0 +1,15 @@
+package sistemas.de.veiculo;
+
+public class SistemasDeVeiculo {
+
+    public static void main(String[] args) {
+        Carro carro = new Carro("Toyota", "Corolla", 4, 2022);
+        Moto moto = new Moto("Honda", "CB500", 500);
+
+        carro.exibirDetalhes();
+        moto.exibirDetalhes();
+
+        carro.definirDetalhes("2025");
+        carro.exibirDetalhes();
+    }
+}
---
+++ Sistemas de Veiculo/src/sistemas/de/veiculo/Veiculo.java
@@ -0,0 +1,18 @@
+package sistemas.de.veiculo;
+
+abstract class Veiculo {
+
+    protected String marca;
+    private final String modelo;
+
+    public Veiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public abstract void exibirDetalhes();
+}
